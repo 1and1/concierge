c28f020de852a48dc9191ed7426fa6ceeb0912fe
bitionaire <johann.boehler@googlemail.com>
2016-04-05 22:07:14 +0200

    log and explicit npe

--- src/test/java/net/oneandone/concierge/JsonHelper.java
+++ src/test/java/net/oneandone/concierge/JsonHelper.java
@@ -1,17 +1,28 @@
 package net.oneandone.concierge;
 
+import org.slf4j.LoggerFactory;
+
 import javax.json.Json;
 import javax.json.JsonReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.net.URL;
 
 public interface JsonHelper {
 
     public static String getJsonStringFor(final Class testClass, final String test) throws IOException {
-        try (final InputStream stream = JsonHelper.class.getResourceAsStream("/json/" + testClass.getSimpleName() + "/" + test + ".json");
-             final Reader reader = new InputStreamReader(stream)) {
+        final String jsonPath = "/json/" + testClass.getSimpleName() + "/" + test + ".json";
+        LoggerFactory.getLogger(JsonHelper.class).info("requesting json resource '{}'", jsonPath);
+
+        final URL jsonUrl = JsonHelper.class.getResource(jsonPath);
+        if (jsonUrl == null) {
+            throw new NullPointerException("resource '" + jsonPath + "' not found");
+        }
+
+        try (final InputStream stream = jsonUrl.openStream();
+             final Reader reader = new InputStreamReader(stream, "utf-8")) {
             final JsonReader jsonReader = Json.createReader(reader);
             return jsonReader.read().toString();
         }
